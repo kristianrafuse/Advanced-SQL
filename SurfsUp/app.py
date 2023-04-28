7e4e899072b5b94e2c7a89963dd1dded39f71a31
kristianrafuse <kristianrafuse@gmail.com>
2023-04-28 15:47:34 -0300

    almost done!
    working.

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -26,17 +26,24 @@ def welcome():
     """List all available api routes."""
     return (
         f"WELCOME TO THE HOMEPAGE!<br/><br/>"
+
         f"Here are the available routes to peruse:<br/><br/>"
+
         f"Precipitation data available at: <a href='http://127.0.0.1:5000/api/v1.0/precipitation'>Precipitation Data</a><br/><br/>"
+
         f"Station data (id, station, name) available at: <a href='http://127.0.0.1:5000/api/v1.0/stations'>Station Data</a><br/><br/>"
+
         f"Temperature data for the most active station available at: <a href='http://127.0.0.1:5000/api/v1.0/tobs'>Temperature Data for Most Active Station</a><br/><br/>"
+
         f"INTERACTIVE! WOW!!<br/><br/>"
-        f"Data available by selecting your start date!: <a href='http://127.0.0.1:5000/api/v1.0/'>Start Date Range Query</a><br/><br/>"
-        f"Enter dates as yyyy-mm-dd like this:<br/><br/>"
-        f"http://127.0.0.1:5000/api/v1.0/api/v1.0/yyyy-mm-dd<br/><br/>"
-        f"Data available by selecting your start date and end date!: <a href='http://127.0.0.1:5000/api/v1.0'>Start Date and End Date Range Query</a><br/><br/>"
-        f"Enter dates as yyyy-mm-dd like this:<br/><br/>"
-        f"http://127.0.0.1:5000/api/v1.0/api/v1.0/yyyy-mm-dd/yyyy-mm-dd<br/><br/>"
+
+        f"Data available by selecting your start date!: <a href='http://127.0.0.1:5000/api/v1.0/temp/2016-08-23'>Start Date Range Query</a><br/><br/>"
+
+        f'For a specified start, returns the minimum, average, and maximum temperatures for all the dates greater than or equal to the start date.<br/><br/>'
+
+        f"Data available by selecting your start date and end date!: <a href='http://127.0.0.1:5000/api/v1.0/temp/2016-08-23/2016-11-31'>Start Date and End Date Range Query</a><br/><br/>"
+
+        f"For a specified start date and end date, returns the minimum, average, and maximum temperatures for the dates from the start date to the end date, inclusive."
 )
 
 # 2. Convert the query results from your precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value.
@@ -80,25 +87,21 @@ def stations():
 def tobs():
     session = Session(engine)
 
+    #get the most recent date from the measurement table the date one year prior to that date.
+
     most_recent_date = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
     most_recent_datetime = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
     one_year_from_most_recent = most_recent_datetime - dt.timedelta(days=365)
+
+    #queries the measurement table to get the count of measurements for each station in descending order
 
     station_counts = session.query(measurement.station, func.count(measurement.station))\
                         .group_by(measurement.station)\
                         .order_by(func.count(measurement.station).desc())\
                         .all()
 
-    sel = [measurement.station, 
-       func.min(measurement.tobs), 
-       func.max(measurement.tobs), 
-       func.avg(measurement.tobs)]
-
-    session.query(*sel)\
-    .filter(measurement.station == "USC00519281", measurement.date)\
-    .group_by(measurement.station)\
-    .order_by(measurement.station)\
-    .all()
+    # Queries the measurement table to get the temperature observations for the most active station for the previous year of data.
+    # The temperature data is stored in a list and returned as a JSON object.
 
     sel = [measurement.tobs, measurement.date]
 
@@ -113,6 +116,64 @@ def tobs():
         'message': f'The most active station is:{station_counts[0]}. Details below:',
         'temperature_data': temp_data
     })
+
+@app.route("/api/v1.0/temp/<start>")
+def temp_start(start):
+    session = Session(engine)
+
+    #select the min, max, and avg values as required from the measurement data
+
+    sel = [func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)]
+
+    #query for table for the data base on measurement.date based on start date variable as data
+
+    temperature_data = session.query(*sel)\
+        .filter(measurement.date >= start)\
+        .all()
+
+    session.close()
+
+    #loop through the data and populate list to JSONIFY!
+
+    temp_list = []
+    for min_temp, avg_temp, max_temp in temperature_data:
+        temp_dict = {}
+        temp_dict["min_temp"] = min_temp
+        temp_dict["avg_temp"] = avg_temp
+        temp_dict["max_temp"] = max_temp
+        temp_list.append(temp_dict)
+
+    return jsonify(temp_list)
+
+#same as above with additional filter on measurement.date based on end date variable
+
+@app.route("/api/v1.0/temp/<start>/<end>")
+def tempstartend(start, end=None):
+    session = Session(engine)
+
+    sel = [func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)]
+
+    if end:
+        temperature_data = session.query(*sel)\
+            .filter(measurement.date >= start)\
+            .filter(measurement.date <= end)\
+            .all()
+    else:
+        temperature_data = session.query(*sel)\
+            .filter(measurement.date >= start)\
+            .all()
+
+    session.close()
+
+    temp_list = []
+    for min_temp, avg_temp, max_temp in temperature_data:
+        temp_dict = {}
+        temp_dict["min_temp"] = min_temp
+        temp_dict["avg_temp"] = avg_temp
+        temp_dict["max_temp"] = max_temp
+        temp_list.append(temp_dict)
+
+    return jsonify(temp_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
